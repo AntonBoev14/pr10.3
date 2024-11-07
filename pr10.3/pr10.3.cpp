c74228960191489c2469994180b1c42b71dc41c9
AntonBoev14 <antonboev421@gmail.com>
2024-11-07 10:10:45 +0300

    Add files via upload

---
+++ pr10.3/pr10.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath> 
+#define M_PI 3.14
+using namespace std;
+
+void calculateCircleArea(double radius, double& area) {
+    area = M_PI * radius * radius; 
+}
+
+int main() {
+    setlocale(0, "ru");
+    double radius1, radius2;
+    double area1, area2;
+
+    cout << "Введите радиус первого круга: ";
+    cin >> radius1;
+
+   cout << "Введите радиус второго круга: ";
+   cin >> radius2;
+
+    calculateCircleArea(radius1, area1);
+    calculateCircleArea(radius2, area2);
+
+   cout << "Площадь круга с радиусом " << radius1 << " равна: " << area1 << endl;
+   cout << "Площадь круга с радиусом " << radius2 << " равна: " << area2 << endl;
+
+    return 0;
+}
